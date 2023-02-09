064e8047d6654d05a97aaca61ce41c29741661c2
cowbuno <talipov.zhenis@gmail.com>
2023-02-09 13:56:32 +0600

    db connection

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,19 @@
 package com.company;
 
+import java.sql.Connection;
+import java.sql.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("test by Zhenis");
-        System.out.println("Test by Nurkhat");
+        DbFunciton db=new DbFunciton();
+        Connection conn=db.connect_to_db("postgres","postgres","12345678");
+        //db.createTable(conn,"employee");
+        //db.insert_row(conn,"employee","Rajat","India");
+        //db.update_name(conn,"employee","Rahul","Raj");
+        //db.search_by_name(conn,"employee","abhishek");
+        //db.delete_row_by_name(conn,"employee","abhishek");
+        //db.delete_row_by_id(conn,"employee",4);
+        //db.read_data(conn,"employee");
+        // write your code here
     }
 }
