dd5087c61bf72464a4abbd8138a4ed3c26d57e95
cowbuno <talipov.zhenis@gmail.com>
2023-02-10 16:27:22 +0600

    Db insert first stage

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,15 +5,9 @@ import java.sql.*;
 public class Main {
 
     public static void main(String[] args) {
-        DbFunciton db=new DbFunciton();
-        Connection conn=db.connect_to_db("postgres","postgres","12345678");
-        //db.createTable(conn,"employee");
-        //db.insert_row(conn,"employee","Rajat","India");
-        //db.update_name(conn,"employee","Rahul","Raj");
-        //db.search_by_name(conn,"employee","abhishek");
-        //db.delete_row_by_name(conn,"employee","abhishek");
-        //db.delete_row_by_id(conn,"employee",4);
-        //db.read_data(conn,"employee");
-        // write your code here
+        DbFunciton db = new DbFunciton();
+        Connection conn = db.connect_to_db("postgres","postgres","12345678");
+        //db.createTable(conn, "consumer ");
+        db.insert_row(conn, "employee", "abhishek", "india");
     }
 }
