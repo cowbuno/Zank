5966cfd88bf1ce5a945e586ccde7d7cf64394524
cowbuno <talipov.zhenis@gmail.com>
2023-02-16 10:00:33 +0600

    Registration

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,14 @@ import java.sql.*;
 public class Main {
 
     public static void main(String[] args) {
-        DbFunciton db = new DbFunciton();
+        DbFunction db = new DbFunction();
         Connection conn = db.connect_to_db("postgres","postgres","12345678");
 //        db.createTable(conn, "consumer ");
 //        db.insert_row(conn, "consumer", "Nurkhat", "nkwt", "nktwn@gmail.com", "050509551608", "123456789" );
 //        db.update_name(conn, "consumer", "Nurkhat", "Zhenis");
 //        db.read_data(conn, "consumer");
 //        System.out.println(db.checkAccount(conn,"consumer", "Zhenis", "123456789"));
+        userRegistration reg = new userRegistration();
+        reg.register();
     }
 }
