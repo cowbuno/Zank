d8902314da33555fbc4ddd8d4933e0c9cfbac836
nktwn <nurkhat.sergaziev@mail.ru>
2023-02-16 09:41:58 +0600

    by Nurkhat

--- src/com/company/DbFunciton.java
+++ src/com/company/DbFunciton.java
@@ -22,49 +22,51 @@ public class DbFunciton {
         }
         return conn;
     }
-    public void createTable(Connection conn, String table_name){
+
+    public void createTable(Connection conn, String table_name) {
         Statement statement;
         try {
             String query = "create table " + table_name + "(empid SERIAL, name varchar(20), surname varchar(20), email varchar(30) ,IIN varchar(12), password varchar(20), primary key(empid));";
             statement = conn.createStatement();
             statement.executeUpdate(query);
             System.out.println("Table created");
-        }catch (Exception e){
-            System.out.println(e);
-        }
-    }
-    public void insert_row(Connection conn,String table_name, String name, String surname, String email, String IIN, String password){
-        Statement statement;
-        try {
-            String query=String.format("insert into %s(name, surname, email, IIN, password) values('%s','%s','%s','%s','%s');",table_name, name, surname, email, IIN, password);
-            statement=conn.createStatement();
-            statement.executeUpdate(query);
-            System.out.println("Row Inserted");
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
 
-    public void read_data(Connection conn, String table_name){
+    public void insert_row(Connection conn, String table_name, String name, String surname, String email, String IIN, String password) {
+        Statement statement;
+        try {
+            String query = String.format("insert into %s(name, surname, email, IIN, password) values('%s','%s','%s','%s','%s');", table_name, name, surname, email, IIN, password);
+            statement = conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Row Inserted");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void read_data(Connection conn, String table_name) {
         Statement statement;
         ResultSet rs = null;
         try {
             String query = String.format("select * from %s", table_name);
             statement = conn.createStatement();
             rs = statement.executeQuery(query);
-            while(rs.next()){
+            while (rs.next()) {
                 System.out.print(rs.getString("name") + " ");
                 System.out.print(rs.getString("surname") + " ");
                 System.out.print(rs.getString("email") + " ");
                 System.out.println(rs.getString("IIN") + " ");
 
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
 
-    public static boolean checkAccount(Connection conn, String table_name, String name, String password){
+    public static boolean checkAccount(Connection conn, String table_name, String name, String password) {
         try {
             String query = String.format("select * from %s where name='%s' and password='%s'", table_name, name, password);
             Statement statement = conn.createStatement();
@@ -73,20 +75,20 @@ public class DbFunciton {
             if (rs.next()) {
                 return true;
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
         return false;
     }
 
-    public void update_name(Connection conn, String table_name, String old_name, String new_name){
+    public void update_name(Connection conn, String table_name, String old_name, String new_name) {
         Statement statement;
         try {
             String query = String.format("update %s set name='%s' where name = '%s'", table_name, new_name, old_name);
             statement = conn.createStatement();
             statement.executeUpdate(query);
             System.out.println("Data update");
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
